219b84d5add6396c0a981b38d526d875961b24f0
luchassmed <kapot@live.dk>
2024-05-06 23:41:22 +0200

    comment google maps

--- Components/MapGoogle.js
+++ Components/MapGoogle.js
@@ -1,37 +1,37 @@
-"use client";
+// "use client";
 
-import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+// import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
-const mapStyles = {
-  width: "100%",
-  height: "50%",
-};
+// const mapStyles = {
+//   width: "100%",
+//   height: "50%",
+// };
 
-const MapGoogle = () => {
-  return (
-    //The <Map></Map> need the following props
-    //initialCenter={} will be the center on the Map
-    <Map
-      google={window.google}
-      zoom={17}
-      style={mapStyles}
-      initialCenter={{
-        lat: 55.68748900618472,
-        lng: 12.589557009063478,
-      }}
-    >
-      //The Maker Component have a prop positio={}
-      //in which you decide the position of it
-      <Marker
-        position={{
-          lat: 55.68748900618472,
-          lng: 12.589557009063478,
-        }}
-      />
-    </Map>
-  );
-};
+// const MapGoogle = () => {
+//   return (
+//     //The <Map></Map> need the following props
+//     //initialCenter={} will be the center on the Map
+//     <Map
+//       google={window.google}
+//       zoom={17}
+//       style={mapStyles}
+//       initialCenter={{
+//         lat: 55.68748900618472,
+//         lng: 12.589557009063478,
+//       }}
+//     >
+//       //The Maker Component have a prop positio={}
+//       //in which you decide the position of it
+//       <Marker
+//         position={{
+//           lat: 55.68748900618472,
+//           lng: 12.589557009063478,
+//         }}
+//       />
+//     </Map>
+//   );
+// };
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyCOEPhDlDYn9JEmJofaFP53KPKEKzaOHxg",
-})(MapGoogle);
+// export default GoogleApiWrapper({
+//   apiKey: "AIzaSyCOEPhDlDYn9JEmJofaFP53KPKEKzaOHxg",
+// })(MapGoogle);
