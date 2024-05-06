1b9ce94ea6e029530f9ad30d4e9ae699c4053337
luchassmed <kapot@live.dk>
2024-05-06 23:47:37 +0200

    favicon

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Pranzo",
-  description: "Pranzo",
+  title: "Pranzo invitation",
+  description: "Pranzo invitation",
 };
 
 export default function RootLayout({ children }) {
