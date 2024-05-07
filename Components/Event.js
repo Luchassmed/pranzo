9686bd0d507a2277f598752cb2b7712353337a35
luchassmed <kapot@live.dk>
2024-05-07 20:39:40 +0200

    google maps i frame

--- Components/Event.js
+++ Components/Event.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Maps from "./MapGoogle";
+import Iframe from "react-iframe";
 
 export default function Event() {
   return (
@@ -18,7 +18,16 @@ export default function Event() {
         </a>
       </div>
       <div className="text-center">
-        {/* <Maps /> */} Google Maps Widget er Undervejs...
+        {/* Google Maps Widget er Undervejs... */}
+        <Iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2249.090323194148!2d12.5896767!3d55.687417399999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4652531f6b9ea14f%3A0x19f4cf53612ed6eb!2sGernersgade%203%2C%201319%20K%C3%B8benhavn!5e0!3m2!1sda!2sdk!4v1715106990278!5m2!1sda!2sdk"
+          width="600"
+          height="300"
+          style="border:0;"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></Iframe>
       </div>
     </div>
   );
