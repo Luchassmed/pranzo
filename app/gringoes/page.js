2c3fb712beb207828b29a1d49b9105952a60ecf4
Luchassmed <kapot@live.dk>
2025-01-27 15:57:58 +0100

    imports?

--- app/gringoes/page.js
+++ app/gringoes/page.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Hero from "../../components/Hero.js";
-import Event from "../../components/Event.js";
-import Invite from "../../components/Invite.js";
-import Footer from "../../components/Footer.js";
+import Hero from "../../components/Hero";
+import Event from "../../components/Event";
+import Invite from "../../components/Invite";
+import Footer from "../../components/Footer";
 
 export default function page() {
   return (
